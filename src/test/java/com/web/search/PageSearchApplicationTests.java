8ee310b4dd457c29e744562b1050cebd93df5ad8
balajimurugesan2016 <balajimurugesan.2016@gmail.com>
2021-05-31 11:03:59 +0530

    Added tests

--- src/test/java/com/web/search/PageSearchApplicationTests.java
+++ src/test/java/com/web/search/PageSearchApplicationTests.java
@@ -1,13 +1,29 @@
 package com.web.search;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.web.search.model.PageData;
+import com.web.search.service.SearchService;
 
 @SpringBootTest
 class PageSearchApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void getPageData() {
+		SearchService ss = new SearchService();
+		List<PageData> pagedatalist = ss.searchresource("for");
+		
+		assertThat(pagedatalist.size()).isEqualTo(1);
+		
 	}
 
 }
